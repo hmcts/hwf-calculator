611c5fe0db38bc092dc92aed13aa973e6605ef25
Gary Taylor <gary.taylor@hismessages.com>
2018-01-02 19:02:10 +0000

    Temporarily changed the number of children page to be valid when on the final page - added comment to fix it as part of RST-670

--- test_common/page_objects/en/calculator/number_of_children_page.rb
+++ test_common/page_objects/en/calculator/number_of_children_page.rb
@@ -2,7 +2,12 @@ module Calculator
   module Test
     module En
       class NumberOfChildrenPage < BasePage
-        set_url '/calculation/number_of_children'
+        # @TODO Number of children has not been defined yet - it is part of another feature
+        # So, the URL below is deliberately WRONG, but the correct URL is in a comment below it
+        # @TODO Review and correct this as part of RST-670.  I have added a comment in 670 to ensure it is addressed
+        # although it should be naturally as the tests should start failing once the next page is added
+        set_url '/calculation/full_remission_available'
+        #set_url '/calculation/number_of_children'
         element :next_button, :button, 'Next step'
 
         def next
