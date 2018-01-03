b577cc8426e2afc631dc18db1628fd024a8a7abe
Alexa <alexa.ballantine@gmail.com>
2018-01-03 17:46:45 +0000

    removed unused step

--- features/step_definitions/savings_and_investment_steps.rb
+++ features/step_definitions/savings_and_investment_steps.rb
@@ -2,7 +2,3 @@ And(/^I submit (?:my|our) savings and investments$/) do
   calculator_disposable_capital_page.disposable_capital.set(user.disposable_capital)
   calculator_disposable_capital_page.next
 end
-
-# When(/^I click on the Next step button on the savings and investment page$/) do
-#   calculator_disposable_capital_page.next
-# end
