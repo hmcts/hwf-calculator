bc7b85cf36a0a3cdd9c061633f9272728d9396e3
Gary Taylor <gary.taylor@hismessages.com>
2018-01-03 10:11:47 +0000

    Added comment linking ticket RST-733

--- app/services/household_income_calculator_service.rb
+++ app/services/household_income_calculator_service.rb
@@ -12,6 +12,7 @@ class HouseholdIncomeCalculatorService < BaseCalculatorService
     self
   end
 
+  # @TODO Review the method below - see details in RST-733, but allow for now
   def self.fields_required(inputs, previous_calculations:)
     if previous_calculations.dig(:benefits_received_calculator_service, :help_available)
       []
