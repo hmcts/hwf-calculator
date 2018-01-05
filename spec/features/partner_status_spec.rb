663ef0d978a4e1d4071e07e83df51eec75995555
Gary Taylor <gary.taylor@hismessages.com>
2018-01-05 08:06:05 +0000

    Added specs for RST-721

---
+++ spec/features/partner_status_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+# This feature represents the acceptance criteria defined in RST-721
+RSpec.describe 'Partner Status Test', type: :feature, js: true do
+  # Feature: Partner Status
+  #
+  # Ability for citizen to confirm their Partner status
+  #
+  #
+  # Personas:
+  #
+  #   JOHN is a single, 56 year old man with 1 child. He is not on any benefit. He has £2,990 worth of capital and an income of £1,330. He has a court fee of £600
+  #   ALLI is a married, 60 year old man with 1 child. He is not on any benefit. He has £3,800 worth of capital and an income of £1,489. He has a court fee of £1,334
+  #
+  #
+  # Scenario: JOHN confirms he is single
+  # Given I am John
+  # And I am on the Partner Status page
+  # And I select Single option
+  # When I click Next Step
+  # Then I should see the next page
+  let(:next_page) { Calculator::Test::En::CourtFeePage.new }
+  scenario 'John confirms he is single' do
+    # Arrange
+    given_i_am(:john)
+    answer_up_to_marital_status_question
+
+    # Act
+    marital_status_page.marital_status.set(messaging.t('hwf_pages.marital_status.labels.marital_status.single'))
+    marital_status_page.next
+
+    # Assert
+    expect(next_page).to be_displayed
+  end
+
+  #
+  # Scenario: ALLI confirms he is married
+  # Given I am Alli and partner
+  # And I am on the Partner Status page
+  # And I select Married or living with someone option
+  # When I click Next Step
+  # Then I should see the next page
+  #
+  scenario 'Alli confirms he is married' do
+    # Arrange
+    given_i_am(:alli)
+    answer_up_to_marital_status_question
+
+    # Act
+    marital_status_page.marital_status.set(messaging.t('hwf_pages.marital_status.labels.marital_status.sharing_income'))
+    marital_status_page.next
+
+    # Assert
+    expect(next_page).to be_displayed
+  end
+end
