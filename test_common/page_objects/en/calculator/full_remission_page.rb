95207d6cd0420e5c654736359c1002aedcb76b5e
Gary Taylor <gary.taylor@hismessages.com>
2018-01-05 11:24:04 +0000

    Messaging on full remission page working for singles and married people

--- test_common/page_objects/en/calculator/full_remission_page.rb
+++ test_common/page_objects/en/calculator/full_remission_page.rb
@@ -2,7 +2,22 @@ module Calculator
   module Test
     module En
       class FullRemissionPage < BasePage
+        include ActiveSupport::NumberHelper
         set_url '/calculation/full_remission_available'
+        section :decision_messaging, '[data-behavior=calculator_feedback]' do
+          element :header, '[data-behavior=calculator_feedback_header]'
+          element :message, '[data-behavior=calculator_feedback_message]'
+        end
+
+        def valid_for_final_positive_message?(user)
+          marital_status = user.marital_status.downcase
+          expected_header = messaging.translate "hwf_decision.final.#{marital_status}.positive.heading"
+          expected_detail = messaging.translate "hwf_decision.final.#{marital_status}.positive.detail",
+            fee: number_to_currency(user.fee, precision: 0, unit: '£'),
+            total_income: number_to_currency(user.monthly_gross_income, precision: 0, unit: '£')
+          feedback_message_with_header(expected_header) &&
+            feedback_message_with_detail(expected_detail)
+        end
       end
     end
   end
