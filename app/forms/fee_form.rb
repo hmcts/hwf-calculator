0f3794b705749014d0cf31512d6de3970cd73632
Gary Taylor <gary.taylor@hismessages.com>
2018-01-08 17:36:33 +0000

    Type of fee form changed to integer (docs only)

--- app/forms/fee_form.rb
+++ app/forms/fee_form.rb
@@ -1,7 +1,10 @@
 # A form object for the court fee
+# Note that this form captures the fee as an integer, but exports it as a float
+# this allows the underlying calculation engine to use floats for precision (i.e. rounding at the end of,
+# not during calculations)
 class FeeForm < BaseForm
   # @!attribute [rw] fee
-  #   @return [Float] The court fee
+  #   @return [Integer] The court fee
   attribute :fee, :strict_integer
 
   validates :fee, numericality: { only_integer: true }
