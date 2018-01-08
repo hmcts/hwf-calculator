f3be163267f51089f88d3306e9076625e04ff319
Gary Taylor <gary.taylor@hismessages.com>
2018-01-08 17:21:19 +0000

    Added 'strict integer type', as the standard active model integer will try to convert anything into an integer and give the result zero if it cannot, which is no good for validation

---
+++ app/types/strict_integer_type.rb
@@ -0,0 +1,10 @@
+class StrictIntegerType < ActiveModel::Type::Integer
+  NUMERIC_RE = /\A\s*\d+\s*\z/
+
+  def cast(value)
+    if value.is_a?(String) && NUMERIC_RE.match(value).nil?
+      return value
+    end
+    super
+  end
+end
