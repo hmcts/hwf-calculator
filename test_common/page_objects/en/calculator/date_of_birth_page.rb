29f9071c9f11301b62d233415e50c409db36e787
Gary Taylor <gary.taylor@hismessages.com>
2018-01-08 10:49:03 +0000

    Added specs to prove functionality of RST-723

--- test_common/page_objects/en/calculator/date_of_birth_page.rb
+++ test_common/page_objects/en/calculator/date_of_birth_page.rb
@@ -2,6 +2,7 @@ module Calculator
   module Test
     module En
       class DateOfBirthPage < BasePage
+        set_url '/calculation/date_of_birth'
         section :date_of_birth, ::Calculator::Test::QuestionDateSection, :calculator_question, 'What is your date of birth?'
         element :next_button, :button, 'Next step'
 
