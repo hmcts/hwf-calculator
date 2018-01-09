26f24d61b6266f110b42fe66ebb1f1b16ffd997e
Gary Taylor <gary.taylor@hismessages.com>
2018-01-09 06:50:43 +0000

    Added validation to total income form spec

--- spec/forms/total_income_form_spec.rb
+++ spec/forms/total_income_form_spec.rb
@@ -3,7 +3,95 @@ RSpec.describe TotalIncomeForm, type: :model do
   subject(:form) { described_class.new }
 
   describe 'validations' do
-    it 'should have some but dont know what they are yet @TODO'
+    context 'disposable_capital' do
+      it 'allows numeric values' do
+        # Arrange
+        form.total_income = '10000'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors).not_to include :total_income
+      end
+
+      it 'disallows blank' do
+        # Arrange
+        form.total_income = ''
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :blank)
+      end
+
+      it 'disallows negative values' do
+        # Arrange
+        form.total_income = '-100'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :greater_than_or_equal_to, count: 0)
+      end
+
+      it 'disallows floating point numbers' do
+        # Arrange
+        form.total_income = '1000.00'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :not_an_integer)
+      end
+
+      it 'disallows non numeric values' do
+        # Arrange
+        form.total_income = 'dddd'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :not_a_number)
+      end
+
+      it 'disallows NaN for security measures' do
+        # Arrange
+        form.total_income = 'NaN'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :not_a_number)
+      end
+
+      it 'disallows Infinity for security measures' do
+        # Arrange
+        form.total_income = 'Infinity'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :not_a_number)
+      end
+
+      it 'disallows -Infinity for security measures' do
+        # Arrange
+        form.total_income = '-Infinity'
+
+        # Act
+        form.valid?
+
+        # Assert
+        expect(form.errors.details[:total_income]).to contain_exactly a_hash_including(error: :not_a_number)
+      end
+    end
   end
 
   describe 'type' do
