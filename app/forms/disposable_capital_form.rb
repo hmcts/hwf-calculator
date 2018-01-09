8e89dc6ef837be975290277d02fe1b81538c0d89
Gary Taylor <gary.taylor@hismessages.com>
2018-01-09 06:13:06 +0000

    Implementation of RST-675 Added errors to view - disposable capital Added translations Added validation to disposable capital form

--- app/forms/disposable_capital_form.rb
+++ app/forms/disposable_capital_form.rb
@@ -1,8 +1,12 @@
 # A form object for the disposable capital question
 class DisposableCapitalForm < BaseForm
   # @!attribute [rw] disposable_capital
-  #   @return [Float] The disposable capital
-  attribute :disposable_capital, :float
+  #   @return [Integer] The disposable capital
+  attribute :disposable_capital, :strict_integer
+
+  validates :disposable_capital, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 0, allow_blank: true
+  }
 
   # The form type
   #
@@ -15,7 +19,7 @@ class DisposableCapitalForm < BaseForm
 
   def export_params
     {
-      disposable_capital: disposable_capital
+      disposable_capital: disposable_capital.to_f
     }
   end
 end
