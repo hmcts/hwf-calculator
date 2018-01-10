6b2b0c50cf111b247a458ac375bd25089158c374
Gary Taylor <gary.taylor@hismessages.com>
2018-01-10 11:13:31 +0000

    Cucumber tests now using the shared helper for answering the date of birth question

--- features/step_definitions/marital_status_steps.rb
+++ features/step_definitions/marital_status_steps.rb
@@ -1,4 +1,4 @@
 And(/^I answer the marital status question$/) do
-  calculator_marital_status_page.marital_status.set(user.marital_status)
-  calculator_marital_status_page.next
+  marital_status_page.marital_status.set(user.marital_status)
+  marital_status_page.next
 end
--- features/step_definitions/savings_and_investment_steps.rb
+++ features/step_definitions/savings_and_investment_steps.rb
@@ -1,4 +1,4 @@
 And(/^I submit (?:my|our) savings and investments$/) do
-  calculator_disposable_capital_page.disposable_capital.set(user.disposable_capital)
-  calculator_disposable_capital_page.next
+  disposable_capital_page.disposable_capital.set(user.disposable_capital)
+  disposable_capital_page.next
 end
