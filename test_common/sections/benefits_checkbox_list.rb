7f2ee643aad3c7049776a1a19aa927ebf55d1f77
Gary Taylor <gary.taylor@hismessages.com>
2018-01-12 16:04:23 +0000

    Implementation of RST-678 - viewing of the benefits received content

--- test_common/sections/benefits_checkbox_list.rb
+++ test_common/sections/benefits_checkbox_list.rb
@@ -1,7 +1,38 @@
 require_relative './question_checkbox_list'
+require_relative 'question_help'
 module Calculator
   module Test
     class BenefitsCheckboxListSection < QuestionCheckboxListSection
+      section :help_section, QuestionHelpSection, :help_section_labelled, 'How benefit affects your claim'
+
+      # Validates that the guidance text is as expected
+      # @param [String, Array[String]] text_or_array Either a single string to (partially) match or an
+      #  array of strings which will be joined by a CR.  Note that whitespace should not be important,
+      # nor html structure etc..
+      # @raise [Capybara::ExpectationNotMet] if the assertion hasn't succeeded during wait time
+      def validate_guidance(text_or_array)
+        strings = Array(text_or_array)
+        help_section.assert_text(strings.join("\n"))
+      end
+
+      def toggle_help
+        help_section.toggle
+      end
+
+      # rubocop:disable Style/PredicateName
+      def has_no_help_text?
+        help_section.help_text_collapsed?
+      end
+
+      def has_help_text?
+        help_section.help_text_expanded?
+      end
+      # rubocop:enable Style/PredicateName
+
+      delegate :wait_for_help_text, to: :help_section
+
+      delegate :wait_for_no_help_text, to: :help_section
+
       def dont_know_guidance
         option = option_labelled(messaging.t('hwf_pages.income_benefits.labels.benefits.dont_know'))
         id_of_guidance = option.guidance_id
