ff82aae935ebe2e630ab762eda3b9bae21e82d1b
Gary Taylor <gary.taylor@hismessages.com>
2018-01-12 09:16:19 +0000

    RST-727 implementation
    This simply ensures the correct english wording is on the date of birth form
    
    (cherry picked from commit 1e0eade)

--- test_common/page_objects/en/calculator/date_of_birth_page.rb
+++ test_common/page_objects/en/calculator/date_of_birth_page.rb
@@ -3,7 +3,8 @@ module Calculator
     module En
       class DateOfBirthPage < BasePage
         set_url '/calculation/date_of_birth'
-        section :date_of_birth, ::Calculator::Test::QuestionDateSection, :calculator_question, 'What is your date of birth?'
+        element :heading, :exact_heading_text, 'Find out if you can get help with fees'
+        section :date_of_birth, ::Calculator::Test::DateOfBirthQuestionSection, :calculator_question, 'What is your date of birth?'
         section :partner_date_of_birth, ::Calculator::Test::QuestionDateSection, :calculator_question, 'What is your partners date of birth?'
         element :next_button, :button, 'Next step'
 
