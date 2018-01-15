3a5ad206d802814f67939cead63b500b13f45ec1
Gary Taylor <gary.taylor@hismessages.com>
2018-01-15 14:56:47 +0000

    Added disclaimer and welsh link for RST-701 (requirements changed)

--- spec/features/landing_page_content_spec.rb
+++ spec/features/landing_page_content_spec.rb
@@ -1,26 +1,93 @@
 require 'rails_helper'
 # This feature represents the acceptance criteria defined in RST-701
 RSpec.describe 'Partner status content', type: :feature, js: true do
+
+=begin
   # Feature: HwF Eligibility Calculator Landing page Content
   #
   #
   #
-  # Scenario: View and validate Calculator Landing Page content
+  # Scenario: View Heading, Body, Disclaimer and Welsh translation link on the Calculator Landing Page (Revised)
   #              Given I am on the HwF Calculator landing page
-  #              When I view the content on the page
-  #             Then Heading should read "Find out if you could get help with fees?"
-  #             And body should display information about the calculator
+  #              When I view the Heading, Body, Disclaimer and Welsh translation link (Revised)
+  #              Then Heading should read "Find out if you could get help with fees"
+  #              And body should display information about the calculator
+  #              And Disclaimer display "This isn't the application and ......." (New)
+  #              And Welsh translation link display "This guide is also available in Welsh (Cymraeg)" (New)
   #
   #
-  scenario 'View and validate Calculator Landing Page content' do
+  #
+  # Heading (Revised)
+  #
+  # Find out if you could get help with fees
+  #
+  #
+  #
+  # Body: (Revised)**
+  #
+  # This calculator will tell you if you’re likely to get help with fees and how much you could get.
+  #
+  # You may be able to get money off the cost of a court or tribunal fee, or have the fee waived in full, if:
+  #
+  # You don't have much in savings, and
+  # You're receiving certain benefits, or
+  # You're on a low income
+  # If you have a partner, your partner's situation will also apply
+  #
+  # What you’ll need (Revised)**
+  #
+  # You'll need information about your (and your partner's, if applicable):
+  #
+  #  total savings amount, and
+  #  either: list of benefits (or proof of certain benefits at the time you paid your fee)
+  #  or: total monthly income"
+  #
+  #
+  # Disclaimer  (New)
+  #
+  # This isn't the application and is designed to give an indicative response only. You'll need................
+  #
+  #
+  #
+  # Welsh Translation link (New)**
+  #
+  # This guide is also available in Welsh (Cymraeg)
+  #
+  #
+  #
+  # NOTE to DEV (New)**
+  #
+  # "in Welsh" should be a link that enables the welsh translation of the calculator
+=end
+
+
+
+
+
+  # Feature: HwF Eligibility Calculator Landing page Content
+  #
+  #
+  #
+  # Scenario: View Heading, Body, Disclaimer and Welsh translation link on the Calculator Landing Page (Revised)
+  #              Given I am on the HwF Calculator landing page
+  #              When I view the Heading, Body, Disclaimer and Welsh translation link (Revised)
+  #              Then Heading should read "Find out if you could get help with fees"
+  #              And body should display information about the calculator
+  #              And Disclaimer display "This isn't the application and ......." (New)
+  #              And Welsh translation link display "This guide is also available in Welsh (Cymraeg)" (New)
+  #
+  #
+  scenario 'View Heading, Body, Disclaimer and Welsh translation link on the Calculator Landing Page' do
     # Act
     start_page.load_page
 
     # Assert
-    aggregate_failures 'Validating headers and content' do
+    aggregate_failures 'Validating headers, content, disclaimer and welsh link' do
       expect(start_page).to have_heading
       expect(start_page.validate_introduction).to be true
       expect(start_page.validate_requirements).to be true
+      expect(start_page.validate_disclaimer).to be true
+      expect(start_page.validate_welsh_link).to be true
     end
 
   end
@@ -51,4 +118,15 @@ RSpec.describe 'Partner status content', type: :feature, js: true do
   #  or: total monthly income"
   #
   #
+
+  # Disclaimer  (New)
+  #
+  # This isn't the application and is designed to give an indicative response only. You'll need................
+  #
+  #
+  #
+  # Welsh Translation link (New)**
+  #
+  # This guide is also available in Welsh (Cymraeg)
+
 end
