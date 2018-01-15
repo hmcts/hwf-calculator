c724d56fb18fa2874ed148fadf56aade2b1bf56a
Gary Taylor <gary.taylor@hismessages.com>
2018-01-15 14:56:47 +0000

    Added disclaimer and welsh link for RST-701 (requirements changed)

--- test_common/page_objects/en/calculator/start_page.rb
+++ test_common/page_objects/en/calculator/start_page.rb
@@ -10,6 +10,9 @@ module Calculator
         element :heading, :exact_heading_text, 'Find out if you could get help with fees'
         element :introduction, '[data-behavior=introduction]'
         element :requirements, '[data-behavior=requirements]'
+        element :disclaimer, '[data-behavior=disclaimer]'
+        element :available_in_welsh, '[data-behavior=welsh_link]'
+        element :welsh_link, '[data-behavior=welsh_link] a'
         element :start_button, :link_or_button, 'Start now'
 
         # Begin a calculator session
@@ -30,6 +33,23 @@ module Calculator
         # @return [Boolean] Should be true
         def validate_requirements
           requirements.assert_text messaging.t('hwf_pages.home.requirements_text')
+          true
+        end
+
+        # Validates that the disclaimer text is correct for the english language
+        # @raise [Capybara::ExpectationNotMet] if the text wasn't found in the correct place
+        # @return [Boolean] Should be true
+        def validate_disclaimer
+          disclaimer.assert_text messaging.t('hwf_pages.home.disclaimer_text')
+          true
+        end
+
+        # Validates that the welsh link is correct for the english language
+        # @raise [Capybara::ExpectationNotMet] if the text wasn't found in the correct place
+        # @return [Boolean] Should be true
+        def validate_welsh_link
+          available_in_welsh.assert_text messaging.t('hwf_pages.home.welsh_link.full_text')
+          welsh_link.assert_text messaging.t('hwf_pages.home.welsh_link.link_text')
           true
         end
       end
