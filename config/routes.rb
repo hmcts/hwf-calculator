81b0fc7c9a5ac15afe1e72b5c4ceee84ca8493e2
Gary Taylor <gary.taylor@hismessages.com>
2018-01-16 13:22:37 +0000

    Added path to calculation resource in routes to allow the updated form to be reloaded as a get (i.e. refresh button pressed in browser)
    (cherry picked from commit d5dca71)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root controller: :calculation, action: :home
-  resource :calculation, controller: :calculation, only: [:update]
+  resource :calculation, controller: :calculation, only: [:update], path: '/calculation/:form'
   get '/calculation/:form' => "calculation#edit", as: :edit_calculation
   get '/calculation_result/full_remission' => 'calculation_result#full_remission_available', as: :calculation_result_full
   get '/calculation_result/partial_remission' => 'calculation_result#partial_remission_available', as: :calculation_result_partial
