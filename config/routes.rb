f07a6aa9ccbb4a2b77764bad448a0c6cfe985a47
Gary Taylor <gary.taylor@hismessages.com>
2018-01-16 13:52:48 +0000

    Changes to routing and views to allow any forms to be refreshed once they have an error in them (at present, it 500's)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root controller: :calculation, action: :home
-  resource :calculation, controller: :calculation, only: [:update], path: '/calculation/:form'
+  resource :calculation, controller: :calculation, only: [:update]
   get '/calculation/:form' => "calculation#edit", as: :edit_calculation
-
+  patch '/calculation/:form' => 'calculation#update', as: :update_calculation
+  patch '/calculation' => 'calculation#update', as: :start_calculation
 end
