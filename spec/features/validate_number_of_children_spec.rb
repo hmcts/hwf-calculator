6a84bdaa6db6d32ad56d0c772b03c0505dc755a3
garytaylor <gary.taylor@hismessages.com>
2018-01-17 10:35:14 +0000

    Feature/rst 752 dev - Validation for number of children (#19)
    * Added feature spec for RST-752
    
    * Implementation for RST-752
    All just normal active model validation stuff

---
+++ spec/features/validate_number_of_children_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+# This feature represents the acceptance criteria defined in RST-752
+RSpec.describe 'Validate court fee test', type: :feature, js: true do
+  let(:next_page) { Calculator::Test::En::TotalIncomePage.new }
+  # Personas
+  #
+  # JOHN is a single, 56 year old man with 1 child
+  # ALLI is a married, 60 year old man with 1 child
+  # OLIVER is a married, 75 year old man with 4 children.
+  #
+  #
+  # Scenario: Enter valid value in the Number of children field
+  #         Given I am JOHN
+  #         And I am on the Number of children question
+  #         And I enter numeric value in the field
+  #         When I click on the Next step button
+  #         Then I should see the next question
+  scenario 'Enter valid value in the Number of children field' do
+    # Arrange
+    given_i_am(:john)
+    answer_questions_up_to_number_of_children
+
+    # Act
+    answer_number_of_children_question
+
+    # Assert
+    expect(next_page).to be_displayed
+  end
+  #
+  #
+  #
+  # Scenario: Enter invalid value in the Number of children field
+  #         Given I am ALLI
+  #         And I am on the Number of children question
+  #         And I enter non-numeric value in the field
+  #         When I click on the Next step button
+  #         Then I should see an error message
+  #
+  #       Error Message: "You must enter the number of financially dependent children"
+  scenario 'Enter invalid value in the Number of children field' do
+    # Arrange
+    given_i_am(:alli)
+    answer_questions_up_to_number_of_children
+
+    # Act
+    number_of_children_page.number_of_children.set('£')
+    number_of_children_page.next
+
+    # Assert
+    expect(number_of_children_page.error_with_text(messaging.t('hwf_pages.number_of_children.errors.non_numeric'))).to be_present
+  end
+  #
+  # Scenario: Number of children field empty - Blank Field (no entry)
+  #         Given I am OLIVER
+  #         And I am on the Number of children question
+  #         And I leave Number of children field blank
+  #         When I click on the Next step button
+  #         Then I should see an error message
+  #
+  #      Error Message: "You must enter the number of financially dependent children"
+  scenario 'Number of children field empty - Blank Field (no entry)' do
+    # Arrange
+    given_i_am(:alli)
+    answer_questions_up_to_number_of_children
+
+    # Act
+    number_of_children_page.next
+
+    # Assert
+    expect(number_of_children_page.error_with_text(messaging.t('hwf_pages.number_of_children.errors.blank'))).to be_present
+  end
+end
--- spec/forms/number_of_children_form_spec.rb
+++ spec/forms/number_of_children_form_spec.rb
@@ -3,7 +3,82 @@ RSpec.describe NumberOfChildrenForm, type: :model do
   subject(:form) { described_class.new }
 
   describe 'validations' do
-    it 'should have some but dont know what they are yet @TODO'
+    it 'allows numeric values' do
+      # Arrange
+      form.number_of_children = '2'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).not_to include :number_of_children
+    end
+
+    it 'disallows floating point numbers' do
+      # Arrange
+      form.number_of_children = '2.00'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
+
+    it 'disallows non numeric values' do
+      # Arrange
+      form.number_of_children = 'dddd'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
+
+    it 'disallows NaN for security measures' do
+      # Arrange
+      form.number_of_children = 'NaN'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
+
+    it 'disallows Infinity for security measures' do
+      # Arrange
+      form.number_of_children = 'Infinity'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
+
+    it 'disallows -Infinity for security measures' do
+      # Arrange
+      form.number_of_children = '-Infinity'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
+
+    it 'disallows -1' do
+      # Arrange
+      form.number_of_children = '-1'
+
+      # Act
+      form.valid?
+
+      # Assert
+      expect(form.errors).to include :number_of_children
+    end
   end
 
   describe 'type' do
