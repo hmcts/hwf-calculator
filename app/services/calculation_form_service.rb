521b913a270e2422ceeb47e356abe81da1714eba
Gary Taylor <gary.taylor@hismessages.com>
2018-01-22 07:40:26 +0000

    Calcilation form service can now lookup a form from a field.  The first form to have the attribute defined is returned

--- app/services/calculation_form_service.rb
+++ app/services/calculation_form_service.rb
@@ -32,4 +32,19 @@ class CalculationFormService
     raise ArgumentError, "Unknown form class for '#{form}'" if klass.nil?
     klass
   end
+
+  # Returns a form class who is the first in the chain to be responsible for the given field symbol
+  #
+  # @param [Symbol, Nil] form The form symbol or nil
+  #
+  # @return [NilForm,MaritalStatusForm,FeeForm,DateOfBirthForm,
+  #   DisposableCapitalForm,BenefitsReceivedForm,NumberOfChildrenForm] The form found
+  # @raise [ArgumentError] If the field specified was not found in any forms
+  def self.for_field(field)
+    form = FORM_CLASSES.values.find do |form|
+      form.has_attribute?(field)
+    end
+    raise ArgumentError, "Field #{field} was not managed by any of the registered forms" if form.nil?
+    form
+  end
 end
