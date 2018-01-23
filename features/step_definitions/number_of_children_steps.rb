6dea13fdfe98dbd17558b40dd004c7f123efbaca
Alexa <alexa.ballantine@gmail.com>
2018-01-23 09:27:43 +0000

    RST-730 added steps for marital status

--- features/step_definitions/number_of_children_steps.rb
+++ features/step_definitions/number_of_children_steps.rb
@@ -1,23 +1,25 @@
 Given("I am on the of children page") do
-  step 'I am Lola and partner'
+  step 'I am Claude and partner'
   step 'I start a new calculator session'
   step 'I answer the marital status question'
   step 'I answer the court fee question'
   step 'I answer the date of birth question'
   step 'I submit my savings and investments'
-  step 'I submit the page with income related benefit checked'
+  step 'I answer the income benefits question'
 end
 
 When("I successfully submit my number of children") do
+  binding.pry
   number_of_children_page.number_of_children.set(user.number_of_children)
   number_of_children_page.next
 end
 
-Then("on the next page I should see my previous answer with our number of children") do |int|
+Then("on the next page I should see my previous answer with our number of children") do
   # TODO: add when functionality is complete
 end
 
 When("I click on children who might affect your claim") do
+  binding.pry
   # TODO: add when functionality is complete
 end
 
@@ -30,6 +32,5 @@ When("I click next without submitting my number of children") do
 end
 
 Then("I should see the number of children error message") do
-  binding.pry
-  expect(number_of_children_page.error_with_text(messaging.t('hwf_pages.number_of_children_page.errors.blank'))).to be_present
+  expect(number_of_children_page.error_with_text(messaging.t('hwf_pages.number_of_children.errors.blank'))).to be_present
 end
