d751524c1fb8d51bdca2f888630fb4504a67a611
Gary Taylor <gary.taylor@hismessages.com>
2018-01-29 15:42:53 +0000

    Revert "Experiment to see if caching of page objects is causing the obsolete node issues"
    This reverts commit e9a9120

--- test_common/helpers/pages.rb
+++ test_common/helpers/pages.rb
@@ -2,58 +2,58 @@ module Calculator
   module Test
     module Pages
       def start_page
-        Calculator::Test::En::StartPage.new
+        @start_page ||= Calculator::Test::En::StartPage.new
       end
 
       def marital_status_page
-        Calculator::Test::En::MaritalStatusPage.new
+        @marital_status_page ||= Calculator::Test::En::MaritalStatusPage.new
       end
 
       def court_fee_page
-        Calculator::Test::En::CourtFeePage.new
+        @court_fee_page ||= Calculator::Test::En::CourtFeePage.new
       end
 
       def date_of_birth_page
-        Calculator::Test::En::DateOfBirthPage.new
+        @date_of_birth_page ||= Calculator::Test::En::DateOfBirthPage.new
       end
 
       def disposable_capital_page
-        Calculator::Test::En::DisposableCapitalPage.new
+        @disposable_capital_page ||= Calculator::Test::En::DisposableCapitalPage.new
       end
 
       # @return [Calculator::Test::En::NumberOfChildrenPage] The number of children page object
       def number_of_children_page
-        Calculator::Test::En::NumberOfChildrenPage.new
+        @number_of_children_page ||= Calculator::Test::En::NumberOfChildrenPage.new
       end
 
       # @return [Calculator::Test::En::IncomeBenefitsPage] The benefits page object
       def income_benefits_page
-        Calculator::Test::En::IncomeBenefitsPage.new
+        @income_benefits_page ||= Calculator::Test::En::IncomeBenefitsPage.new
       end
 
       # @return [Calculator::Test::En::TotalIncomePage] The total income page object
       def total_income_page
-        Calculator::Test::En::TotalIncomePage.new
+        @total_income_page ||= Calculator::Test::En::TotalIncomePage.new
       end
 
       # @return [Calculator::Test::En::NotEligiblePage] The not eligible page object
       def not_eligible_page
-        Calculator::Test::En::NotEligiblePage.new
+        @not_eligible_page ||= Calculator::Test::En::NotEligiblePage.new
       end
 
       # @return [Calculator::Test::En::FullRemissionPage] The full remission page object
       def full_remission_page
-        Calculator::Test::En::FullRemissionPage.new
+        @full_remission_page ||= Calculator::Test::En::FullRemissionPage.new
       end
 
       # @return [Calculator::Test::En::PartialRemissionPage] The partial remission page object
       def partial_remission_page
-        Calculator::Test::En::PartialRemissionPage.new
+        @partial_remission_page ||= Calculator::Test::En::PartialRemissionPage.new
       end
 
       # @return [Calculator::Test::En::BasePage] The base page object which applies to any page
       def any_calculator_page
-        ::Calculator::Test::En::BasePage.new
+        @any_calculator_page ||= ::Calculator::Test::En::BasePage.new
       end
     end
   end
