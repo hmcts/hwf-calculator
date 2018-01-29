e82cd018ed2acaae7641c6f459a47a1deed28cd2
Alexa <alexa.ballantine@gmail.com>
2018-01-29 14:17:06 +0000

    RST-730 tests for landing page

---
+++ features/step_definitions/landing_page_steps.rb
@@ -0,0 +1,19 @@
+Given("I am on the landing page") do
+  start_page.load_page
+end
+
+Then("I should see information about the help with fees calculator") do
+  expect(start_page).to have_heading
+  expect(start_page.validate_introduction).to be true
+  expect(start_page.validate_requirements).to be true
+  expect(start_page.validate_disclaimer).to be true
+  expect(start_page.validate_welsh_link).to be true
+end
+
+When("I click on start now") do
+  start_page.start_session
+end
+
+Then("I am taken to the next page") do
+  expect(marital_status_page).to be_displayed
+end
