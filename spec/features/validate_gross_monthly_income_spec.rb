419cd7f2e788e86d1baa2bafd33e42fcd03fea63
Gary Taylor <gary.taylor@hismessages.com>
2018-01-30 09:19:27 +0000

    Changes from RST-681 copied across - all text changes for error messaging

--- spec/features/validate_gross_monthly_income_spec.rb
+++ spec/features/validate_gross_monthly_income_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 # This feature represents the acceptance criteria defined in RST-681
 RSpec.describe 'Validate gross monthly income test', type: :feature, js: true do
+  # Personas
+  #
+  # JOHN is a single, 56 year old man with 1 child. He is not on any benefit. He has £2,990 worth of capital and an income of £1,330. He has a court fee of £600
+  # JAMES is a single, 45 year old man with 0 children. He is not on income benefit. He has £4,999 worth of capital and an income of £1,084. He has a court fee of £1,664
+  # SUE is a married, 75 year old woman with 0 children. She is not on any benefit. She has £9,999 worth of capital and an income of £0. He has a court fee of £4,000
+  #
+  #
   # Feature: Validate Total Income field
   # HwF Eligibility Calculator should be able to validate the total income field at the form level
   #
-  # @TODO Implement the scenario below as part of RST-744 then remove this comment
-  # Scenario: Total Income field tool tip
-  #             Given user navigate to the Total Income question
-  #             And focus is away from the total income field
-  #             When user view the total income field
-  #             Then tooltip is displayed in the field
-  #      #Tooltip content: "£ Please enter a number between 0 and 100,000 (no decimal places)".
-  #      #Proposed content: "£ Enter numeric value (no decimal places)"
-  #
-  # Scenario: Specify numeric value in Total Income field
-  #             Given user navigate to the Total Income question
-  #             And click in the total income field
-  #             When user enter numeric value in the field
-  #             And click on the Next step button
+  # Scenario: Specify valid value in Total Income field (Revised)
+  #             Given I am JOHN
+  #             And I am on the total income page
+  #             And I enter a valid amount in the field
+  #             When I click on the Next step button
   #             Then the value in the field is validated
-  #             And Total Income response displayed
-  #      #Default value on field selection: "£"
+  #             And I should see total income response
   scenario 'Specify numeric value in Total Income field' do
     # Arrange
     given_i_am(:john)
@@ -34,17 +31,19 @@ RSpec.describe 'Validate gross monthly income test', type: :feature, js: true do
     expect(full_remission_page).to be_displayed
   end
 
-  # Scenario: Specify non-numeric value in Total Income field
-  #             Given user navigate to the Total Income question
-  #             And click in the total income field
-  #             When user enter non-numeric value in the field
-  #             And click on the Next step button
+  # Scenario: Specify invalid value in Total Income field (Revised)
+  #
+  #             Given I am JAMES
+  #             And I am on the total income page
+  #             And I enter invalid input in the field
+  #             When I click on the Next step button
   #             Then the value in the field is validated
-  #             And error message is displayed prompting user to enter numeric value
-  #       #Error Message: "Please enter numeric value with no decimal places"
+  #             And I should see an error message
+  #
+  #       Error Message: "Please enter a valid value
   scenario 'Specify non-numeric value in Total Income field' do
     # Arrange
-    given_i_am(:john)
+    given_i_am(:james)
     answer_up_to(:total_income)
 
     # Act
@@ -55,16 +54,17 @@ RSpec.describe 'Validate gross monthly income test', type: :feature, js: true do
     expect(total_income_page.error_with_text(messaging.t('hwf_pages.total_income.errors.non_numeric'))).to be_present
   end
 
-  # Scenario: Validate empty Total Income field
-  #             Given user navigate to the total income question
-  #             And click in the total income field
-  #             When user leave total income field empty
-  #             And click on the Next step button
-  #             Then error message is displayed prompting user to enter numeric value
-  #       #Error Message: "Please enter numeric value with no decimal places"
+  # Scenario: Validate empty Total Income field (Revised)
+  #             Given I am JAMES
+  #             And I am on the total income page
+  #             And I leave total income field empty
+  #             When I click on the Next step button
+  #             Then I should see an error message
+  #
+  #       Error Message: "Please enter a valid value
   scenario 'Validate empty Total Income field' do
     # Arrange
-    given_i_am(:john)
+    given_i_am(:james)
     answer_up_to(:total_income)
 
     # Act
