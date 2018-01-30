fbd6cb87779c62a70dda09c5425c54ac8f46657d
Gary Taylor <gary.taylor@hismessages.com>
2018-01-30 09:34:37 +0000

    QA check of change RST-675 - validate savings and investments - wording changes and a test fix

--- spec/features/validate_savings_and_investments_spec.rb
+++ spec/features/validate_savings_and_investments_spec.rb
@@ -1,28 +1,18 @@
 require 'rails_helper'
 # This feature represents the acceptance criteria defined in RST-675
 RSpec.describe 'Validate savings and investment test', type: :feature, js: true do
-  let(:next_page) { Calculator::Test::En::NumberOfChildrenPage.new }
+  let(:next_page) { Calculator::Test::En::IncomeBenefitsPage.new }
 
   # Feature: Validate Savings & Investments field
   # Validate the Savings and Investments field at the form level
   #
-  # @TODO Implement the scenario below as part of RST-744 then remove this comment
-  # Scenario: Savings & Investments field tool tip
-  #              Given user navigate to the savings and investments question
-  #              And focus is away from the savings and investments field
-  #              When user view the savings and investments field
-  #              Then user should see tooltip displayed in the field
-  #      Tooltip content: "£ Enter valid value with no decimal places"
   #
-  #
-  # Scenario: Enter valid value in Savings & Investments field
-  #              Given user navigate to the savings and investments question
-  #              And click in the savings & investments field
-  #              When user enter valid value in the field
-  #              And click on the Next step button
+  # Scenario: Enter valid value in Savings & Investments field (Revised)
+  #              Given I am on the savings and investments question
+  #              And I enter valid value in the savings & investments field
+  #              When I click on the Next step button
   #              Then the value in the field is validated
-  #              And user should see disposable capital response
-  #      Default value on field selection: "£"
+  #              And I should see the next question
   scenario 'Enter valid value in Savings & Investments field' do
     # Arrange
     given_i_am(:john)
@@ -33,16 +23,15 @@ RSpec.describe 'Validate savings and investment test', type: :feature, js: true 
     disposable_capital_page.next
 
     # Assert
-    expect(next_page).to be_present
+    expect(next_page).to be_displayed
   end
-  # Scenario: Enter invalid value in Savings & Investments field
-  #              Given user navigate to the savings and investments question
-  #              And click in the savings & investments field
-  #              When user enter invalid value in the field
-  #              And click on the Next step button
-  #              Then the value in the field is validated
-  #              And user should see error message
-  #       Error Message: "Please enter a valid value. The two nearest valid values are: XX and XX."
+  # Scenario: Enter invalid value in Savings & Investments field (Revised)
+  #            Given  I am on the savings and investments question
+  #             And I enter invalid value in the savings & investments field
+  #             When I click on the Next step button
+  #            Then the value in the field is validated
+  #            And I should see error message
+  # Error Message: "Enter a valid value"
   scenario 'Enter invalid value in Savings & Investments field' do
     # Arrange
     given_i_am(:john)
@@ -55,13 +44,14 @@ RSpec.describe 'Validate savings and investment test', type: :feature, js: true 
     # Assert
     expect(disposable_capital_page.error_with_text(messaging.t('hwf_pages.disposable_capital.errors.non_numeric'))).to be_present
   end
-  # Scenario: Savings & Investments field is empty/blank - no entry
-  #              Given user navigate to the savings and investments question
-  #              And click in the savings & investments field
-  #              When user leave Savings & Investments field blank
-  #              And click on the Next step button
-  #              Then user should see error message
-  #       Error Message: "Enter how much you have in savings and investments"
+  # Scenario: Savings & Investments field is empty/blank - no entry (Revised)
+  #             Given  I am on the savings and investments question
+  #             And I leave the field blank
+  #             When I click on the Next step button
+  #             Then the value in the field is validated
+  #             And I should see error message
+  #
+  #     Error Message: "Enter how much you have in savings and investments"
   scenario 'Savings & Investments field is empty/blank - no entry' do
     # Arrange
     given_i_am(:john)
