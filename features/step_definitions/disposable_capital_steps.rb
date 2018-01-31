32bfa0d53274bd6299b3db11cfe712526eb268be
Gary Taylor <gary.taylor@hismessages.com>
2018-01-31 15:08:51 +0000

    Refactored features so that the translation is done inside page objects

--- features/step_definitions/disposable_capital_steps.rb
+++ features/step_definitions/disposable_capital_steps.rb
@@ -2,21 +2,14 @@ include ActiveSupport::NumberHelper
 
 Then(/^I should see that (?:I am|we are) likely to get help with fees$/) do
   marital_status = user.marital_status.downcase
-  msg = messaging.translate("hwf_decision.disposable_capital.#{marital_status}.positive.detail",
-    fee: number_to_currency(user.fee, precision: 0, unit: '£'),
-    disposable_capital: number_to_currency(user.disposable_capital, precision: 0, unit: '£'))
-  expect(any_calculator_page.feedback_message_with_header(messaging.translate("hwf_decision.disposable_capital.#{marital_status}.positive.heading"))).to be_present
-  expect(any_calculator_page.feedback_message_with_detail(msg)).to be_present
+  expect(any_calculator_page).to have_feedback_message_with_header(:"disposable_capital.#{marital_status}.positive")
+  expect(any_calculator_page).to have_feedback_message_with_detail(:"disposable_capital.#{marital_status}.positive", fee: user.fee, disposable_capital: user.disposable_capital)
 end
 
 Then(/^I should see that (?:I am|we are) unlikely to get help with fees$/) do
   marital_status = user.marital_status.downcase
-  msg = messaging.translate("hwf_decision.disposable_capital.#{marital_status}.negative.detail",
-    fee: number_to_currency(user.fee, precision: 0, unit: '£'),
-    disposable_capital: number_to_currency(user.disposable_capital, precision: 0, unit: '£'))
-  expect(not_eligible_page).to be_displayed
-  expect(not_eligible_page.feedback_message_with_detail(msg)).to be_present
-  expect(not_eligible_page.feedback_message_with_header(messaging.translate("hwf_decision.disposable_capital.#{marital_status}.negative.heading"))).to be_present
+  expect(any_calculator_page).to have_feedback_message_with_header(:"disposable_capital.#{marital_status}.negative")
+  expect(any_calculator_page).to have_feedback_message_with_detail(:"disposable_capital.#{marital_status}.negative", fee: user.fee, disposable_capital: user.disposable_capital)
 end
 
 Given(/^I am on the savings and investment page$/) do
