3b330ab0bd5f9fc37d653e30450e4857c1346ee0
Gary Taylor <gary.taylor@hismessages.com>
2018-02-05 15:49:10 +0000

    Home page disclaimer now has correct icon

--- spec/features/view_previous_answers_spec.rb
+++ spec/features/view_previous_answers_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
         and(have_no_number_of_children).
         and(have_no_total_income)
       expect(date_of_birth_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(date_of_birth_page.previous_answers.court_fee).to have_answered(user.marital_status)
+      expect(date_of_birth_page.previous_answers.court_fee).to have_answered(user.fee)
     end
   end
 
@@ -83,8 +83,8 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
         and(have_no_number_of_children).
         and(have_no_total_income)
       expect(disposable_capital_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(disposable_capital_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(disposable_capital_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
+      expect(disposable_capital_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(disposable_capital_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
     end
   end
 
@@ -102,9 +102,9 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
         and(have_no_number_of_children).
         and(have_no_total_income)
       expect(disposable_capital_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(disposable_capital_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(disposable_capital_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(disposable_capital_page.previous_answers.partner_date_of_birth).to have_answered(user.marital_status)
+      expect(disposable_capital_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(disposable_capital_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(disposable_capital_page.previous_answers.partner_date_of_birth).to have_answered(user.partner_date_of_birth)
     end
   end
 
@@ -121,9 +121,9 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
         and(have_no_number_of_children).
         and(have_no_total_income)
       expect(income_benefits_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(income_benefits_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(income_benefits_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(income_benefits_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
+      expect(income_benefits_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(income_benefits_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(income_benefits_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
     end
   end
 
@@ -139,10 +139,10 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
       expect(number_of_children_page.previous_answers).to have_no_number_of_children.
         and(have_no_total_income)
       expect(number_of_children_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(number_of_children_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(number_of_children_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(number_of_children_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(number_of_children_page.previous_answers.income_benefits).to have_answered(user.marital_status)
+      expect(number_of_children_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(number_of_children_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(number_of_children_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(number_of_children_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
     end
   end
 
@@ -158,10 +158,10 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
       expect(full_remission_page.previous_answers).to have_no_number_of_children.
         and(have_no_total_income)
       expect(full_remission_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.income_benefits).to have_answered(user.marital_status)
+      expect(full_remission_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(full_remission_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(full_remission_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(full_remission_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
     end
   end
 
@@ -176,11 +176,11 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
     aggregate_failures 'Validating all answers' do
       expect(total_income_page.previous_answers).to have_no_total_income
       expect(total_income_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(total_income_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(total_income_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(total_income_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(total_income_page.previous_answers.income_benefits).to have_answered(user.marital_status)
-      expect(total_income_page.previous_answers.number_of_children).to have_answered(user.marital_status)
+      expect(total_income_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(total_income_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(total_income_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(total_income_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
+      expect(total_income_page.previous_answers.number_of_children).to have_answered(user.number_of_children)
     end
   end
 
@@ -195,12 +195,12 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
     aggregate_failures 'Validating all answers' do
       expect(full_remission_page).to be_displayed
       expect(full_remission_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.income_benefits).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.number_of_children).to have_answered(user.marital_status)
-      expect(full_remission_page.previous_answers.total_income).to have_answered(user.marital_status)
+      expect(full_remission_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(full_remission_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(full_remission_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(full_remission_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
+      expect(full_remission_page.previous_answers.number_of_children).to have_answered(user.number_of_children)
+      expect(full_remission_page.previous_answers.total_income).to have_answered(user.total_income)
     end
   end
 
@@ -215,12 +215,12 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
     aggregate_failures 'Validating all answers' do
       expect(not_eligible_page).to be_displayed
       expect(not_eligible_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.income_benefits).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.number_of_children).to have_answered(user.marital_status)
-      expect(not_eligible_page.previous_answers.total_income).to have_answered(user.marital_status)
+      expect(not_eligible_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(not_eligible_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(not_eligible_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(not_eligible_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
+      expect(not_eligible_page.previous_answers.number_of_children).to have_answered(user.number_of_children)
+      expect(not_eligible_page.previous_answers.total_income).to have_answered(user.total_income)
     end
   end
 
@@ -235,12 +235,12 @@ RSpec.describe 'Change previous answers test', type: :feature, js: true do
     aggregate_failures 'Validating all answers' do
       expect(partial_remission_page).to be_displayed
       expect(partial_remission_page.previous_answers.marital_status).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.court_fee).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.date_of_birth).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.disposable_capital).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.income_benefits).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.number_of_children).to have_answered(user.marital_status)
-      expect(partial_remission_page.previous_answers.total_income).to have_answered(user.marital_status)
+      expect(partial_remission_page.previous_answers.court_fee).to have_answered(user.fee)
+      expect(partial_remission_page.previous_answers.date_of_birth).to have_answered(user.date_of_birth)
+      expect(partial_remission_page.previous_answers.disposable_capital).to have_answered(user.disposable_capital)
+      expect(partial_remission_page.previous_answers.income_benefits).to have_answered(user.income_benefits)
+      expect(partial_remission_page.previous_answers.number_of_children).to have_answered(user.number_of_children)
+      expect(partial_remission_page.previous_answers.total_income).to have_answered(user.total_income)
     end
   end
 end
