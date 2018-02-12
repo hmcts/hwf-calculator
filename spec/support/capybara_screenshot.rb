5c410e0a3f7bfcbb3914f27c01f42607455dbf19
Gary Taylor <gary.taylor@hismessages.com>
2018-02-12 16:21:22 +0000

    Pushing travis a little harder to run more in parallel

--- spec/support/capybara_screenshot.rb
+++ spec/support/capybara_screenshot.rb
@@ -3,3 +3,6 @@ Capybara::Screenshot.prune_strategy = :keep_last_run
 Capybara::Screenshot.register_driver(:chrome) do |driver, path|
   driver.browser.save_screenshot(path)
 end
+Capybara::Screenshot.register_driver(:chromedriver) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
