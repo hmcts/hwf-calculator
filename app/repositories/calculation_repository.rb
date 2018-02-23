d076af1c91a098fc14bb900c506f9b83cb8ba898
Gary Taylor <gary.taylor@hismessages.com>
2018-02-23 12:48:30 +0000

    Added comments to calculation repository

--- app/repositories/calculation_repository.rb
+++ app/repositories/calculation_repository.rb
@@ -1,21 +1,34 @@
+# A repository to persist the calculation to a hash like store
+# the store can be a rails session.
 class CalculationRepository
   KEY = :calculator_repository
 
+  # Creates a new repository backed by a given store
+  # @param [#fetch,#delete,#[]=] store A hash like store that must support fetch, delete and []=
   def initialize(store:)
     self.store = store
   end
 
+  # Finds the single calculation in the repo.  If none existed a new one is created
+  #
+  # @return [Calculation] The calculation instance from the repo
   def find
     data = store.fetch(KEY) do
-      YAML.dump(Calculation.new)
+      encode(Calculation.new)
     end
     decode(data)
   end
 
+
+  # @param [Calculation] calculation The calculation to save
+  # @return [Boolean] true if successful
   def save(calculation)
     store[KEY] = encode(calculation)
+    true
   end
 
+  # Deletes all calculations from the repository
+  # @return [Calculation] Returns self for chaining
   def delete_all
     store.delete(KEY)
     self
