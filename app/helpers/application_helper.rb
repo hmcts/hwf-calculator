caa68f200a8ce6630963cd0fce111c86ccedc62f
garytaylor <gary.taylor@hismessages.com>
2018-03-06 04:26:39 -0800

    Gem updates (#56)
    * Gem updates
    
    * Codeclimate test reporter was well out of date
    
    * Codeclimate test reporter was not even required - travis has its own

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,8 +41,8 @@ module ApplicationHelper
     link_to(t('language_switcher.link_text'), locale: locale_param_for(t('language_switcher.switch')))
   end
 
-  def locale_param_for(t)
-    t == 'en' ? nil : t
+  def locale_param_for(lang)
+    lang == 'en' ? nil : lang
   end
 
   private
