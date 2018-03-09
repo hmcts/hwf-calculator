65080eb098359d94fb1253c5898f9d18c1833f28
Gary Taylor <gary.taylor@hismessages.com>
2018-03-09 18:12:09 +0000

    RST-919 - Now, the calculation is in an un initialized state before it is first updated.  Any attempts to fetch a question page whilst in this state will cause the flow to reset.  This means you can literally use the back button to go back to any page after a final result page and it will just restart

--- app/controllers/calculation_result_controller.rb
+++ app/controllers/calculation_result_controller.rb
@@ -1,6 +1,7 @@
 # This controller handles all final results from the calculator - either full, none or partial
 class CalculationResultController < ApplicationController
   include CalculationStore
+  before_action :start_again, unless: :calculation_state_valid?
   after_action :mark_as_complete
 
   private
@@ -8,5 +9,10 @@ class CalculationResultController < ApplicationController
   def mark_as_complete
     current_calculation.freeze
     repo.save current_calculation
+  end
+
+  def start_again
+    redirect_to root_path
+    false
   end
 end
