2f240a069001bd21869268aad3c2ea69b1c38716
Gary Taylor <gary.taylor@hismessages.com>
2018-03-19 15:05:32 +0000

    RST-947 - Guidance Text Link In IE and Edge
    The GDS standards recommend we use details and summary tags which modern browsers support, so they provided a polyfill for older browsers.
    
    However, this polyfill was not very well written which means it does not play well with pages that are constantly changing (such as any rails app with turbolinks enabled).
    It was also hooking into all click events even ones that it should not care about and intercepting them to see if they are a summary tag that has a parent details tag !!
    
    So, I have re written this and simply not used their code - this now works and is mainly done in CSS.  The javascript simply toggles the 'open' attribute in the details tag.

--- app/assets/javascripts/details.polyfill.js
+++ app/assets/javascripts/details.polyfill.js
@@ -1,3 +1,56 @@
-$(document).ready(function() {
-    GOVUK.details.init();
-});
+;(function (global) {
+    'use strict';
+
+    var $ = global.jQuery;
+    var GOVUK = global.GOVUK || {};
+    var Calculator = global.Calculator || {};
+
+    function DetailsPolyfill () {
+        var self = this;
+
+        self.init = function() {
+            if(checkSupport()) {
+                return;
+            }
+            $(document).on('turbolinks:load', addDetailsPolyfillClass);
+            $(document).on('click', 'details > summary', onSummaryClick);
+        };
+
+        function addDetailsPolyfillClass() {
+            $('details').addClass('details-polyfill');
+        };
+
+        function onSummaryClick(event, a) {
+            var details;
+            event.preventDefault();
+            details = $(event.currentTarget).closest('details');
+            toggleDetails(details);
+            return false;
+        }
+
+        function toggleDetails(details) {
+            var open = details.attr('open');
+            if(open) {
+                details.removeAttr('open');
+            } else {
+                details.attr('open', 'open');
+            }
+        }
+
+        /*
+         * Checks for support for `<details>`
+         */
+
+        function checkSupport () {
+            var el = document.createElement('details');
+            return 'boolean' === typeof el.open;
+        }
+    }
+
+    Calculator.DetailsPolyfill = DetailsPolyfill;
+    global.DetailsPolyfill = DetailsPolyfill;
+    $(document).ready(function() {
+        var detailsPolyfill = new DetailsPolyfill();
+        detailsPolyfill.init();
+    });
+})(window);
