de7e4e65753d92ecaaeb952242caea53dbd2f7c8
garytaylor <gary.taylor@hismessages.com>
2018-03-20 03:47:01 -0700

    Feature/rst 963 (#79)
    * RST-947 - Guidance Text Link In IE and Edge
    
    The GDS standards recommend we use details and summary tags which modern browsers support, so they provided a polyfill for older browsers.
    
    However, this polyfill was not very well written which means it does not play well with pages that are constantly changing (such as any rails app with turbolinks enabled).
    It was also hooking into all click events even ones that it should not care about and intercepting them to see if they are a summary tag that has a parent details tag !!
    
    So, I have re written this and simply not used their code - this now works and is mainly done in CSS.  The javascript simply toggles the 'open' attribute in the details tag.
    
    * RST-947 - Guidance Text Link In IE and Edge
    
    The GDS standards recommend we use details and summary tags which modern browsers support, so they provided a polyfill for older browsers.
    
    However, this polyfill was not very well written which means it does not play well with pages that are constantly changing (such as any rails app with turbolinks enabled).
    It was also hooking into all click events even ones that it should not care about and intercepting them to see if they are a summary tag that has a parent details tag !!
    
    So, I have re written this and simply not used their code - this now works and is mainly done in CSS.  The javascript simply toggles the 'open' attribute in the details tag.
    
    It now also supports aria as the old one did

--- app/assets/javascripts/details.polyfill.js
+++ app/assets/javascripts/details.polyfill.js
@@ -1,3 +1,107 @@
-$(document).ready(function() {
-    GOVUK.details.init();
-});
+;(function (global) {
+    'use strict';
+
+    var $ = global.jQuery;
+    var GOVUK = global.GOVUK || {};
+    var Calculator = global.Calculator || {};
+
+    function DetailsPolyfill () {
+        var self = this;
+        var _nextId = 0;
+        self.init = function() {
+            if(checkSupport()) {
+                return;
+            }
+            $(document).on('turbolinks:load', addDetailsPolyfillClass);
+            $(document).on('turbolinks:load', addAria);
+            $(document).on('click', 'details > summary', onSummaryClick);
+        };
+
+        function addDetailsPolyfillClass() {
+            $('details').addClass('details-polyfill');
+        };
+
+        function onSummaryClick(event, a) {
+            var details;
+            event.preventDefault();
+            details = $(event.currentTarget).closest('details');
+            toggleDetails(details);
+            updateAriaState(details);
+            return false;
+        }
+
+        function toggleDetails(details) {
+            var open = details.attr('open');
+            if(open) {
+                details.removeAttr('open');
+            } else {
+                details.attr('open', 'open');
+            }
+        }
+
+        function addAria() {
+            $('details').each(function() {
+                var details, summary, content;
+                details = $(this);
+                content = details.children(':not(summary)').first();
+                summary = details.children('summary').first();
+                ensureContentHasId(content);
+                addAriaToDetails(details, summary, content);
+                updateAriaState(details);
+                setTabIndex(summary);
+            });
+        }
+
+        function ensureContentHasId(content) {
+            if(content.attr('id') === undefined) {
+                content.attr('id', nextId());
+            }
+        }
+
+        function addAriaToDetails(details, summary, content) {
+            details.attr('role', 'group');
+            summary.attr('role', 'button');
+            summary.attr('aria-controls', content.attr('id'));
+        }
+
+        function updateAriaState(details) {
+            var summary, content;
+            summary = details.children('summary').first();
+            content = details.children(':not(summary)').first();
+            if(details.attr('open')) {
+                summary.attr('aria-expanded', true);
+                content.attr('aria-hidden', false);
+            } else {
+                summary.attr('aria-expanded', false);
+                content.attr('aria-hidden', true);
+            }
+        }
+
+        function setTabIndex(summary) {
+            summary.attr('tabindex', '0');
+
+        }
+
+        function nextId() {
+            _nextId += 1;
+            return 'polyfill-id-' + _nextId;
+
+        }
+
+        /*
+         * Checks for support for `<details>`
+         */
+
+        function checkSupport () {
+            var el = document.createElement('details');
+            return 'boolean' === typeof el.open;
+        }
+    }
+
+    Calculator.DetailsPolyfill = DetailsPolyfill;
+    global.DetailsPolyfill = DetailsPolyfill;
+    $(document).ready(function() {
+        var detailsPolyfill = new DetailsPolyfill();
+        detailsPolyfill.init();
+    });
+})(window);
