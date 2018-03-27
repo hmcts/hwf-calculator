4236cd086cfac22c219f949d1874ce084bd4a0d8
garytaylor <gary.taylor@hismessages.com>
2018-03-27 06:55:46 -0700

    Added more validation to number of children - the max is now 99 ! (#86)

--- test_common/sections/number_of_children_question.rb
+++ test_common/sections/number_of_children_question.rb
@@ -10,6 +10,8 @@ module Calculator
 
       included do
         element :error_non_numeric, :exact_error_text, t("#{i18n_scope}.errors.non_numeric")
+        element :error_too_high, :exact_error_text, t("#{i18n_scope}.errors.too_high")
+        element :error_too_low, :exact_error_text, t("#{i18n_scope}.errors.too_low")
         element :error_blank, :exact_error_text, t("#{i18n_scope}.errors.blank")
       end
     end
