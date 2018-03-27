e94798ed5dba7eecae0d5ea0e6dc7100cfd5163b
Gary Taylor <gary.taylor@hismessages.com>
2018-03-27 07:49:15 +0100

    The total income page now varies its question label based on marital status

--- test_common/page_objects/calculator/total_income_page.rb
+++ test_common/page_objects/calculator/total_income_page.rb
@@ -1,11 +1,23 @@
 module Calculator
   module Test
     class TotalIncomePage < BasePage
+      SINGLE_LABEL = t('hwf_pages.total_income.questions.total_income.label.single')
+      MARRIED_LABEL = t('hwf_pages.total_income.questions.total_income.label.sharing_income')
       set_url t('hwf_urls.total_income')
       element :heading, :exact_heading_text, t('hwf_pages.total_income.heading')
       element :next_button, :button, t('hwf_pages.total_income.buttons.next')
 
-      section :total_income, :calculator_question, t('hwf_pages.total_income.questions.total_income.label') do
+      section :total_income, :calculator_question, [SINGLE_LABEL, MARRIED_LABEL] do
+        @i18n_scope = 'hwf_pages.total_income.questions.total_income'
+        include ::Calculator::Test::TotalIncomeQuestionSection
+      end
+
+      section :total_income_married, :calculator_question, MARRIED_LABEL do
+        @i18n_scope = 'hwf_pages.total_income.questions.total_income'
+        include ::Calculator::Test::TotalIncomeQuestionSection
+      end
+
+      section :total_income_single, :calculator_question, SINGLE_LABEL do
         @i18n_scope = 'hwf_pages.total_income.questions.total_income'
         include ::Calculator::Test::TotalIncomeQuestionSection
       end
