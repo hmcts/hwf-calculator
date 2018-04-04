589b34b99f1d2fe87652067369af51bd7c890189
Gary Taylor <gary.taylor@hismessages.com>
2018-04-04 10:57:10 +0100

    Refactoring slightly

--- test_common/capybara/capybara_screenshot.rb
+++ test_common/capybara/capybara_screenshot.rb
@@ -5,14 +5,19 @@ module Calculator
         changed = false
         orig_size = Capybara.current_session.current_window.size
         begin
-          width  = Capybara.page.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-          height = Capybara.page.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+          width, height = page_size
           Capybara.current_session.current_window.resize_to(width + 100, height + 100)
           changed = true
           yield
         ensure
           Capybara.current_session.current_window.resize_to(*orig_size) if changed
         end
+      end
+
+      def self.page_size
+        width  = Capybara.page.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
+        height = Capybara.page.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+        [width, height]
       end
     end
   end
