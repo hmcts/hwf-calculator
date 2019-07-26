34b39de51ba277ea057739b01e0e00e90fd278b7
tomaszcwikcs <tomasz@cwiksolutions.co.uk>
2019-07-26 11:16:30 +0100

    refactoring

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PHUSION_SERVICE="${PHUSION:-false}"
+case ${PHUSION_SERVICE} in
+true)
+    echo "running serivce"
+    cd /home/app/
+    bundle exec unicorn -p ${UNICORN_PORT:-8080} -c ./config/unicorn.rb -E ${RAILS_ENV:-production}
+    ;;
+*)
+    bundle exec unicorn -p ${UNICORN_PORT:-8080} -c ./config/unicorn.rb
+    ;;
+esac
