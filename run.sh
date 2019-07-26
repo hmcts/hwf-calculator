7fce73f532bab0c386019435df7415cecda57bb7
tomaszcwikcs <tomasz@cwiksolutions.co.uk>
2019-07-26 12:37:53 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 PHUSION_SERVICE="${PHUSION:-false}"
 case ${PHUSION_SERVICE} in
 true)
-    echo "running serivce"
+    echo "running as service"
     cd /home/app/
     bundle exec unicorn -p ${UNICORN_PORT:-8080} -c ./config/unicorn.rb -E ${RAILS_ENV:-production}
     ;;
